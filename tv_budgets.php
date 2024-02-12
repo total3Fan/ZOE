a2776198434db18be758c6f34d0186bc6a2ae5c1
total3Fan <156814380+total3Fan@users.noreply.github.com>
2024-02-12 08:41:23 +0000

    Update tv_budgets.php
    // ADDED following settings
    
    // DO YOU WANT ZOE TO DO HEDGING, WHEN YOU HAVE LONG OPEN YOU WANT IT TO LEAVE OPEN AND ALSO OPEN SHORT?
    // 1 = HAVE BOTH OPEN  0 = CLOSE LONG FOR SHORT TO OPEN (VISA VERSA)
    
    $hedgeTrades = 0;
    
    // STOP LOSS ON FIBS OR PRICE
    // 1 = FIBS  0 = PRICE
    
    $stopLossFIBS = 1;

--- tv_budgets.php
+++ tv_budgets.php
@@ -1,5 +1,6 @@
 <?php
 
+$debug = 1;
 /*#################################################################################
 THIS FILE ALLOWS YOU TO CHANGE SETTINGS FOR THE APPLICATION, PLEASE READ COMMENTS
 © 2023 Zoe is licensed under Attribution-ShareAlike 4.0 International 
@@ -11,7 +12,10 @@ START HERE FOR VARIABLES TO CHANGE OR UPDATE
 
 $leverage 				= 20; 
 
+// DO YOU WANT ZOE TO DO HEDGING, WHEN YOU HAVE LONG OPEN YOU WANT IT TO LEAVE OPEN AND ALSO OPEN SHORT?
+// 1 = HAVE BOTH OPEN  0 = CLOSE LONG FOR SHORT TO OPEN (VISA VERSA)
 
+$hedgeTrades = 0;
 
 
 // COMPOUND SETTINGS 1 = On   0 = Off
@@ -19,11 +23,11 @@ $compound = 1;
 
 // WHAT % BALANCE TO USE FOR BUDGET PER TRADE DEFAULT IS 2% 
 
-$spendBudgetPercent				= 1;
+$spendBudgetPercent				= 2;
 
 // WHAT % OF FUTURES BALANCE IS MAXIMUM TO USE FOR ALL TRADES DEFAULT IS 10% 
 
-$maxTradesPercent				= 10;
+$maxTradesPercent				= 30;
 
 
 // If you set compound to 0 then following settings apply, otherwise will be ignored
@@ -40,13 +44,13 @@ $maxPairings 			= 10;
 // ANY TRADE TIMELINE THAT IS BELOW THIS NUMBER WILL NOT BE LISTED
 // 60 = 1HOUR
 
-$tradeTimelineMin		= 5;
+$tradeTimelineMin		= 1;
 
 // HOW LONG DO YOU WANT TRADES TO SIT IN THE DATABASE FOR ONCE LISTED, AS THEY WILL RETRIGGER IF CLOSED AND WITHIN THIS TIME THRESHOLD
 // IF YOU HAVE 30 THEN THEY WILL BE REMOVED AFTER 30 MINUTES
 // IF YOU HAVE 1 THEN THEY WILL BE REMOVED AFTER 1 MINUTE
 
-$leaveDatabaseTrades 	= 5;
+$leaveDatabaseTrades 	= 2;
 
 
 // WHEN UNREALISED PROFIT IS GREATER THAN THE FOLLOWING IT WILL CLOSE ALL TRADES AND START AGAINST
@@ -54,17 +58,21 @@ $leaveDatabaseTrades 	= 5;
 // 50 = 50% OF BALANCE
 // 10 = 10% OF BALANCE
 
-$cutOffPercent					=  5;
+$cutOffPercent					=  3;
 
+// STOP LOSS ON FIBS OR PRICE
+// 1 = FIBS  0 = PRICE
 
+$stopLossFIBS = 1;
 
 // OVER-RIDE INITIAL STOP LOSS PERCENTAGE
 
 $overRideSL = 1;
+
 // DEFAULT IS 0.5% : YOU CAN PLACE A NEW PERCENTAGE IF DESIRED (EXCLUDES LEVERAGE)
 // STOP IS NOT FROM ENTRY PRICE BUT FROM THE 1 FIB AT THE TIME OF ENTRY
 
-$stopLossPercent = 5; // 5%
+$stopLossPercent = 100; // 5%
 
 
 // OVER-RIDE INITIAL TARGET PERCENTAGE
@@ -75,7 +83,7 @@ $overRideTarget = 1;
 // TARGET IS FROM ENTRY PRICE 
 // ONLY APPLICABLE IF $multiTargets = 0;
 
-$targetPointPercent = 25; // 10%
+$targetPointPercent = 12; // 10%
 
 
 
@@ -95,13 +103,13 @@ $tradeTargetsByTimeline = [
 
 		4 => 10,
 		5 => 10,
-		8 => 10,
-		10 => 10,
-		11 => 10,
-		15 => 20, 
-		30 => 30, 
-		60 => 50, 
-		120 => 200
+		8 => 120,
+		10 => 120,
+		11 => 140,
+		15 => 200, 
+		30 => 300, 
+		60 => 500, 
+		120 => 1000
 		];
 
 
@@ -115,7 +123,7 @@ $tradeShortsByTimeline = [
 		5 => 10,
 		8 => 10,
 		10 => 10,
-		11 => 10,
+		11 => 15,
 		15 => 20, 
 		30 => 30, 
 		60 => 50, 
@@ -213,7 +221,6 @@ $maxPairings 			= number_format($maxPairingsBudget / $spendBudget,0,'.','');
 $cutOff 						= ($futuresBalance * ($cutOffPercent / 100));
 
 
-
 // For short positions
 $stopLossShortFigure = 1 + (($stopLossPercent / $leverage) / 100); // Stop loss for short should be positive
 $targetPointShortFigure = 1 - (($targetPointPercent / $leverage) / 100); // Target for short should be negative
@@ -224,29 +231,53 @@ $targetPointLongFigure = 1 + (($targetPointPercent / $leverage) / 100); // Targe
 
 
 
-
 if($debug == 1)
 {
-	echo '<hr>';
-	echo '<table>';
-	echo '<td>Futures Balance:</td><td>$'.number_format($futuresBalance,0,'.','').'</td></tr>';
-	echo '<td>Leverage:</td><td>x'.$leverage.'</td></tr>';
-	echo '<td>Futures Exposure:</td><td>$'.number_format($futuresExposure,0,'.','').'</td></tr>';
 	
-	if($compound == 1)
-	{
-		echo '<td>Percentage Per Trade:</td><td>'.$spendBudgetPercent.'%'.'</td></tr>';
-		echo '<td>Budget Per Trade:</td><td>$'.$spendBudget.'</td></tr>';
-		echo '<td>Percentage Total Trades:</td><td>'.$maxTradesPercent.'%</td></tr>';
-		echo '<td>Total Trades Budget:</td><td>$'.$maxPairingsBudget.'</td></tr>';
-		echo '<td>Maximum Trades:</td><td>'.$maxPairings.'</td></tr>';
-		echo '<td>STOP LOSS with LEVERAGE: </td><td>'.($stopLossPercent * $leverage).'%</td></tr>';
-		echo '<td>RISK without LEVERAGE: </td><td>$'.($maxPairingsBudget * (($stopLossPercent) / 100)).'</td></tr>';
-		echo '<td>Cut-Off Amount:</td><td>$'.number_format($cutOff,2,'.','').'</td></tr>';
-	}
+	$dateToCheck = date('Y-m-d');
+ // Change this to your desired date
+										
+
+// Create a DateTime object for the dateToCheck
+$dateTime = new DateTime($dateToCheck);
+
+// Loop through the last 7 days up to the dateToCheck
+
+
+$profitToday = calculateProfitOrLoss($dateTime->format('Y-m-d'), $user_bnKey, $user_bnSecret);
+echo '<hr>';
+							
+	echo '<table border="1" width="100%">';
+	echo '<tr>';
+	echo '<td>USER</td>';
+	echo '<td>Percentage Per Trade:</td>';
+	echo '<td>Budget Per Trade:</td>';
+	echo '<td>Percentage Total Trades:</td>';
+	echo '<td>Total Trades Budget:</td>';
+	echo '<td>Maximum Trades:</td>';
+	echo '<td>STOP LOSS with LEVERAGE: </td>';
+	echo '<td>RISK without LEVERAGE: </td>';
+	echo '<td>Cut-Off Amount:</td>';
+	echo '<td>Profit</td>';
+		echo '</tr>';
+	echo '<tr>';
+	echo '<td>'.$user_userID.'</td>';
+	echo '<td>'.$spendBudgetPercent.'%'.'</td>';
+		echo '<td>$'.$spendBudget.'</td>';
+		echo '<td>'.$maxTradesPercent.'%</td>';
+		echo '<td>$'.$maxPairingsBudget.'</td>';
+		echo '<td>'.$maxPairings.'</td>';
+		echo '<td>'.($stopLossPercent).'%</td>';
+		echo '<td>$'.($maxPairingsBudget * (($stopLossPercent) / 100)).'</td>';
+		echo '<td>$'.number_format($cutOff,2,'.','').'</td>';
 	
+	echo '<td>'.number_format($profitToday,2).'</td>';
+	echo '</tr>';
 	echo '</table>';
-	echo '<hr>';
+flush();
+
+	
+	
 }
 
 
@@ -265,7 +296,16 @@ $dateTime = new DateTime($dateToCheck);
 // Loop through the last 7 days up to the dateToCheck
 
 
-
-
+  $sqlUpdateBalance = "UPDATE coinData_user_balances SET 
+        
+        balance_amount_futures = '$futuresBalance',
+        balance_amount_futures_profit = '$profitToday'
+    WHERE balance_userID = '$user_userID'";
+    
+    if ($conn->query($sqlUpdateBalance) === TRUE) {
+       
+    } else {
+        echo "Error: " . $conn->error;
+    }
 
 ?>
